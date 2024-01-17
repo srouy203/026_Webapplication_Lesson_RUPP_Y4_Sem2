d9404cc2171fdfcad8db7dc6a62982a02bb74868
srouy203 <leangsrouyang@gmail.com>
2024-01-17 18:35:22 +0700

    17 Jan 2024

---
+++ 1. first.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title> <!-- output តម្លៃអថេរ title -->
+    
+</head>
+<body>
+    <h1>php tags</h1>
+    <?php 
+        echo "Year 4,E5";
+    ?>
+    <h2>Output directive</h2>
+    <?= "Y4E5" ?>
+</body>
+</html>
